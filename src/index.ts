e144c6609e15d215441d837f646ed2109673e4e3
Muhammad Akbar <m.akbar.pro@gmail.com>
2024-07-25 22:38:53 +0700

    added completed full calendar config files

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,30 @@
-import { greetUser } from '$utils/greet';
+//Before running pnpm dev, make sure to run pnpm install then pnpm add (dependencies)
+
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  window.Wized = window.Wized || [];
+  window.Wized.push((Wized) => {
+    const calendarEl = document.querySelector<HTMLDivElement>('[data-element = "calendar"]');
+    if (!calendarEl) return;
+
+    const events = Wized.data.v.eventsData;
+
+    const calendar = new Calendar(calendarEl, {
+      plugins: [dayGridPlugin, timeGridPlugin],
+      height: 'auto',
+      initialView: 'timeGridDay',
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'timeGridDay,timeGridWeek,dayGridMonth',
+      },
+
+      events: events,
+    });
+    calendar.render();
+  });
 });

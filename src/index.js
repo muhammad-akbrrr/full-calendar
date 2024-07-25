0cdb8d7acb0705fc8469c1c4f051f47384131dbc
Muhammad Akbar <m.akbar.pro@gmail.com>
2024-07-25 22:41:30 +0700

    prettier code format

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@
 //Dont forget to add the <script> and </script> line at the beginning & at the end of the code.
 
 document.addEventListener('DOMContentLoaded', function () {
-    window.Wized = window.Wized || [];
-    window.Wized.push((Wized) => {
-      const events = Wized.data.v.eventsData;
-      const calendarEl = document.querySelector('[data-element = "calendar"]');
-      const calendar = new FullCalendar.Calendar(calendarEl, {
-        height: 'auto',
-        initialView: 'timeGridDay',
-        headerToolbar: {
-          left: 'prev,next today',
-          center: 'title',
-          right: 'timeGridDay,timeGridWeek,dayGridMonth',
-        },
-        events: events,
-      });
-      calendar.render();
+  window.Wized = window.Wized || [];
+  window.Wized.push((Wized) => {
+    const events = Wized.data.v.eventsData;
+    const calendarEl = document.querySelector('[data-element = "calendar"]');
+    const calendar = new FullCalendar.Calendar(calendarEl, {
+      height: 'auto',
+      initialView: 'timeGridDay',
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'timeGridDay,timeGridWeek,dayGridMonth',
+      },
+      events: events,
     });
+    calendar.render();
   });
+});
